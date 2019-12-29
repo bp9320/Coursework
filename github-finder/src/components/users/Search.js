05bfb91fd04cd8e239153a7be1844ffef494df0e
Brian Pirot <brian.pirot@sunquestinfo.com>
2019-12-28 21:45:51 -0600

    refactored app to use context api for app level state. Added home and not found components. End of Project 1

--- github-finder/src/components/users/Search.js
+++ github-finder/src/components/users/Search.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
   const [text, setText] = useState("");
 
+  const { searchUsers, clearUsers, users } = githubContext;
+  const { setAlert } = alertContext;
   const onSubmit = e => {
     e.preventDefault();
     if (text === "") {
@@ -32,20 +38,13 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
           className="btn btn-dark btn-block"
         />
       </form>
-      {showClear && (
+      {users.length > 0 && (
         <button className="btn btn-light btn-block" onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
