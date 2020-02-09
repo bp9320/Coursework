98448812e240ebff553bb6a61a9b6337ca198a7b
Brian Pirot <brian.pirot@sunquestinfo.com>
2020-02-08 23:13:08 -0600

    refactored routing into controllers and routes folders, added env variables, created server.js

--- Udemy/Javascript/Node/Schmedtmann/complete-node-bootcamp-master/4-natours/starter/app.js
+++ Udemy/Javascript/Node/Schmedtmann/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,132 +1,26 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+// MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// ROUTES
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: { tours }
-  });
-};
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 404, message: 'Tour not found.' });
-  }
-
-  res.status(200).json({ status: 'success', data: { tour } });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour data here....>' }
-  });
-};
-
-const addTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour }
-      });
-    }
-  );
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
-  }
-
-  res.status(204).json({ status: 'success', data: null });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Route not defined!' });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Route not defined!' });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Route not defined!' });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Route not defined!' });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({ status: 'error', message: 'Route not defined!' });
-};
-
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.post('/api/v1/tours', addTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(addTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-app
-  .route('/api/v1/users')
-  .get(getAllUsers)
-  .post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
---
+++ Udemy/Javascript/Node/Schmedtmann/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App running on port ${port}...`);
+});
