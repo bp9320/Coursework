4259999f03d96b8e5422354362463eeb9d62cd76
Brian Pirot <brian.pirot@sunquestinfo.com>
2020-02-09 18:49:41 -0600

    add mongoose and mongoDB connection

--- Udemy/Javascript/Node/Schmedtmann/complete-node-bootcamp-master/4-natours/starter/server.js
+++ Udemy/Javascript/Node/Schmedtmann/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,9 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 // console.log(process.env);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB Connection Successful!'));
 
 const port = process.env.PORT || 3000;
 
